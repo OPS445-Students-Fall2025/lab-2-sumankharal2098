250235883bd98cfa05dc8b875e728190363ebaff
Sashank Kharal <skharal3@myseneca.ca>
2025-09-26 18:47:42 -0400

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Sashank 
+# Author ID: 137379236
+# Date Created: 2025/09/26
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
