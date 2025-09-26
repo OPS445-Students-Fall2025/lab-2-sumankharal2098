3820feec6f199dcb3356f9172f0f87eaa55a02bc
Sashank Kharal <skharal3@myseneca.ca>
2025-09-26 18:47:56 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Your Full Name
+# Author ID: YourSenecaID
+# Date Created: 2025/09/26
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
